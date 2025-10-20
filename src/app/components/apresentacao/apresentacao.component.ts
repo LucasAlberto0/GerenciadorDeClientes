926fc7899c0f625f6f5cf598d049cc981d1c4b8a
Lucas Alberto <lucasalberto4321@gmail.com>
2025-10-20 16:55:59 -0300

    feat: login-component

--- src/app/components/apresentacao/apresentacao.component.ts
+++ src/app/components/apresentacao/apresentacao.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-apresentacao',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './apresentacao.component.scss'
 })
 export class ApresentacaoComponent {
+  router = inject(Router);
 
+  NavegarRota() {
+    this.router.navigate(['login']);
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  router = inject(Router);
 
+  NavegarRota() {
+    this.router.navigate(['login'])
+  }
 }
