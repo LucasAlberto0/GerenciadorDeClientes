62755b3bc1795bc14957bd4d56e4d4f4c7c9c0fc
LucasAlberto0 <lucasalberto4321@gmail.com>
2025-11-01 17:05:13 -0300

    ajustando-conteudos-dos-cards-de-qualidades-da-empresa

--- src/app/components/home/qualidades-albertech/qualidades-albertech.component.ts
+++ src/app/components/home/qualidades-albertech/qualidades-albertech.component.ts
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './qualidades-albertech.component.scss'
 })
 export class QualidadesAlbertechComponent {
-  cards = Array(4);
+
+  qualidades = [{
+    imagem: 'img/icons/dados-icon.svg',
+    titulo: 'Dados Seguros',
+    descricao: 'Armazenamento seguro com backup automático e proteção total'
+  },
+  {
+    imagem: 'img/icons/comunidade-icon.svg',
+    titulo: 'Gestão Completa',
+    descricao: 'Cadastre, edite e organize todos os seus clientes em uma interface moderna'
+  },
+  {
+    imagem: 'img/icons/grafico-icon.svg',
+    titulo: 'Relatórios Inteligentes',
+    descricao: 'Visualize métricas e estatísticas em tempo real com gráficos interativos'
+  },
+  {
+    imagem: 'img/icons/seguranca-icon.svg',
+    titulo: 'Segurança Avançada',
+    descricao: 'Autenticação robusta e criptografia de ponta a ponta'
+  },]
 }
